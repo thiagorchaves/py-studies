c2e7ff69b40a8c824321a7e2151c56abbe9f4479
Thiago Chaves <thiago@qcx.com.br>
2019-03-20 19:36:11 -0300

    fix route

--- pymongo/mensagem/blueprint.py
+++ pymongo/mensagem/blueprint.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 import json
 
-mensagens_routes = Blueprint("grupos", __name__, url_prefix="/mensagens")
+mensagens_routes = Blueprint("mensagem", __name__, url_prefix="/mensagens")
 
 @mensagens_routes.route("")
 def getMensagens():
